9a5c392dc26019c7ad5dad41b5250a4696ce71f7
alliewarren <warrena@carleton.edu>
2017-05-21 21:44:00 -0500

    initial python file

--- decision_tree.py
+++ decision_tree.py
@@ -4,6 +4,10 @@ Aidan Holloway-Bidwell and Allie Warren
 Data Mining
 Final Project
 Decision Trees
+
+choose attribute with greatest standard deviation reduction
+split the data on the attribute
+recurse
 '''
 def main():
 
